3089a1f5bd175d76db94094d4f11f1b8932bada8
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2012-03-14 19:49:14 +0000

    Added classes for repeating background textures.

---
+++ src/org/pandcorps/pandax/visual/Pantexture.java
@@ -0,0 +1,63 @@
+/*
+Copyright (c) 2009-2011, Andrew M. Martin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+   disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of Pandam nor the names of its contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.pandcorps.pandax.visual;
+
+import org.pandcorps.pandam.*;
+
+public class Pantexture extends Panctor {
+	protected final Panmage image;
+	protected int offX = 0;
+	protected int offY = 0;
+	protected int width = 0;
+	protected int height = 0;
+	
+	public Pantexture(final String id, final Panmage image) {
+		super(id);
+		this.image = image;
+	}
+	
+	public void setOffset(final int offX, final int offY) {
+		this.offX = offX;
+		this.offY = offY;
+	}
+	
+	public void setSize(final int width, final int height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	@Override
+	protected void updateView() {		
+	}
+
+	@Override
+	public Pansplay getCurrentDisplay() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	protected void renderView(final Panderer renderer) {
+		final Panple pos = getPosition();
+		renderer.render(getLayer(), image, pos.getX(), pos.getY(), pos.getZ(), offX, offY, width, height);
+	}
+}
---
+++ src/org/pandcorps/pandax/visual/ScrollTexture.java
@@ -0,0 +1,62 @@
+/*
+Copyright (c) 2009-2011, Andrew M. Martin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+   disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of Pandam nor the names of its contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.pandcorps.pandax.visual;
+
+import org.pandcorps.pandam.*;
+import org.pandcorps.pandam.event.*;
+
+public final class ScrollTexture extends Pantexture implements StepListener {
+	private int velX = 0;
+	private int velY = 0;
+	
+	public ScrollTexture(final String id, final Panmage image) {
+		super(id, image);
+	}
+	
+	public final void setVelocity(final int velX, final int velY) {
+		this.velX = velX;
+		this.velY = velY;
+	}
+	
+	@Override
+	public final void onStep(final StepEvent event) {
+		final Panple size = image.getSize();
+		if (velX != 0) {
+			offX = add(offX, velX, size.getX());
+		}
+		if (velY != 0) {
+			offY = add(offY, velY, size.getY());
+		}
+	}
+	
+	private final static int add(int off, final int vel, final float size) {
+		final int s = (int) size;
+		off += vel;
+		if (off < 0) {
+			off += s;
+		} else if (off >= s) {
+			off -= s;
+		}
+		return off;
+	}
+}
