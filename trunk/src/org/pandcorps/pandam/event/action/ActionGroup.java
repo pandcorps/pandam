482ec98201a9758c645eab450cd09aa4f26848d9
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2012-03-26 22:35:48 +0000

    Added ActionGroup operations.

--- trunk/src/org/pandcorps/pandam/event/action/ActionGroup.java
+++ trunk/src/org/pandcorps/pandam/event/action/ActionGroup.java
@@ -2,29 +2,51 @@ package org.pandcorps.pandam.event.action;
 
 import java.util.ArrayList;
 
+import org.pandcorps.core.Coltil;
 import org.pandcorps.pandam.Pangine;
 import org.pandcorps.pandam.Panput;
 import org.pandcorps.pandam.Panteraction;
 
 public class ActionGroup {
-	private final ArrayList<ActionListener> listeners = new ArrayList<ActionListener>();
-    private final ArrayList<ActionStartListener> startListeners = new ArrayList<ActionStartListener>();
+	private ArrayList<ActionListener> listeners = null;
+    private ArrayList<ActionStartListener> startListeners = null;
+    private ArrayList<ActionEndListener> endListeners = null;
     
     public void register(final Panput input, final ActionListener listener) {
         final Panteraction inter = Pangine.getEngine().getInteraction();
-        inter.register(input, listener);
-        listeners.add(listener);
+        inter.register(null, input, listener);
+        add(listener);
+    }
+    
+    public void add(final ActionListener listener) {
+        listeners = Coltil.add(listeners, listener);
     }
     
     public void register(final Panput input, final ActionStartListener listener) {
         final Panteraction inter = Pangine.getEngine().getInteraction();
-        inter.register(input, listener);
-        startListeners.add(listener);
+        inter.register(null, input, listener);
+        add(listener);
+    }
+    
+    public void add(final ActionStartListener listener) {
+        startListeners = Coltil.add(startListeners, listener);
+    }
+    
+    public void register(final Panput input, final ActionEndListener listener) {
+        final Panteraction inter = Pangine.getEngine().getInteraction();
+        inter.register(null, input, listener);
+        add(listener);
+    }
+    
+    public void add(final ActionEndListener listener) {
+        endListeners = Coltil.add(endListeners, listener);
     }
     
     public void unregister() {
     	final Panteraction inter = Pangine.getEngine().getInteraction();
         inter.unregisterAllStart(startListeners);
+        startListeners = null;
         inter.unregisterAll(listeners);
+        listeners = null;
     }
 }
