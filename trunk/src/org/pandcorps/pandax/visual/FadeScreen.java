ddaf3b69bb170832dd098d08ffcd86758e07d787
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2012-03-29 10:57:55 +0000

    Modified FadeScreen to close resources as soon as it's finished with them.

--- trunk/src/org/pandcorps/pandax/visual/FadeScreen.java
+++ trunk/src/org/pandcorps/pandax/visual/FadeScreen.java
@@ -80,8 +80,9 @@ public abstract class FadeScreen extends Panscreen {
             } else {
                 color.setA(oldAlpha);
                 // If we open a new Panscreen, this will be automatic, but do it in case finish does something else
-                c.unregisterListeners();
+                unregisterListeners();
                 finish(); // Might open a new FadeScreen, so revert alpha first
+                destroy();
             }
         }
     }
