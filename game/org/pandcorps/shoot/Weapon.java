517151ec63a206ba6311b06a8fb6cd55639ed775
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2012-09-21 15:14:05 +0000

    Added shell casings.

--- game/org/pandcorps/shoot/Weapon.java
+++ game/org/pandcorps/shoot/Weapon.java
@@ -2,20 +2,25 @@ package org.pandcorps.shoot;
 
 import java.util.HashSet;
 
+import org.pandcorps.core.Mathtil;
 import org.pandcorps.core.Pantil;
 import org.pandcorps.game.actor.Burst;
 import org.pandcorps.pandam.*;
+import org.pandcorps.pandam.event.*;
+import org.pandcorps.pandax.Pandy;
 
 public class Weapon extends Panctor {
 	public final static class WeaponDefinition {
 		private final Panmage image;
 		private final Panimation flashAnm;
+		private final Panimation casingAnm;
 		protected final Emitter[] attackEmitters;
 		protected final Emitter[] attackingEmitters;
 		
-		public WeaponDefinition(final Panmage image, final Panimation flashAnm, final Emitter[] attackEmitters, final Emitter[] attackingEmitters) {
+		public WeaponDefinition(final Panmage image, final Panimation flashAnm, final Panimation casingAnm, final Emitter[] attackEmitters, final Emitter[] attackingEmitters) {
 			this.image = image;
 			this.flashAnm = flashAnm;
+			this.casingAnm = casingAnm;
 			this.attackEmitters = attackEmitters;
 			this.attackingEmitters = attackingEmitters;
 		}
@@ -45,5 +50,27 @@ public class Weapon extends Panctor {
 				getLayer().addActor(flash);
 			}
 		}
+		if (def.casingAnm != null) {
+		    for (final Panple projPos : projPositions) {
+		        final Casing casing = new Casing(def.casingAnm);
+		        casing.getPosition().set(projPos.getX() - 7, projPos.getY(), projPos.getZ());
+		        final float rx = Mathtil.randf(1, 2.5f), ry = Mathtil.randf(2.5f, 4.5f);
+		        casing.getVelocity().set(mirror ? rx : -rx, ry);
+		        casing.getAcceleration().setY(-.75f);
+                getLayer().addActor(casing);
+		    }
+		}
+	}
+	
+	private final static class Casing extends Pandy implements AnimationEndListener {
+        public Casing(final Panimation anm) {
+            super(Pantil.vmid());
+            setView(anm);
+        }
+
+        @Override
+        public final void onAnimationEnd(final AnimationEndEvent event) {
+            destroy();
+        }
 	}
 }
