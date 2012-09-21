d93fb2e575fa02d352d9ecd3e5899b5367e545cc
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2012-09-21 00:07:35 +0000

    Added a flash to the rocket launcher.

--- trunk/game/org/pandcorps/shoot/Weapon.java
+++ trunk/game/org/pandcorps/shoot/Weapon.java
@@ -1,16 +1,19 @@
 package org.pandcorps.shoot;
 
 import org.pandcorps.core.Pantil;
+import org.pandcorps.game.actor.Burst;
 import org.pandcorps.pandam.*;
 
 public class Weapon extends Panctor {
 	public final static class WeaponDefinition {
 		private final Panmage image;
+		private final Panimation flashAnm;
 		protected final Emitter[] attackEmitters;
 		protected final Emitter[] attackingEmitters;
 		
-		public WeaponDefinition(final Panmage image, final Emitter[] attackEmitters, final Emitter[] attackingEmitters) {
+		public WeaponDefinition(final Panmage image, final Panimation flashAnm, final Emitter[] attackEmitters, final Emitter[] attackingEmitters) {
 			this.image = image;
+			this.flashAnm = flashAnm;
 			this.attackEmitters = attackEmitters;
 			this.attackingEmitters = attackingEmitters;
 		}
@@ -22,5 +25,21 @@ public class Weapon extends Panctor {
 		super(Pantil.vmid());
 		this.def = def;
 		setView(def.image);
+	}
+	
+	protected final void attack(final Shooter shooter, final Emitter[] emitters) {
+		if (emitters == null) {
+			return;
+		}
+		final boolean mirror = isMirror();
+		Panple projPos = null;
+		for (final Emitter em : emitters) {
+		    projPos = em.emit(shooter, mirror).getPosition();
+		}
+		if (def.flashAnm != null) {
+			final Burst flash = new Burst(def.flashAnm);
+			flash.getPosition().set(projPos);
+			getLayer().addActor(flash);
+		}
 	}
 }
