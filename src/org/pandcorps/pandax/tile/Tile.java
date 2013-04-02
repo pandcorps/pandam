47f186d65ce583a57d54b5eaa2fe835b6733246a
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2013-04-02 15:37:38 +0000

    Added a utility method.

--- src/org/pandcorps/pandax/tile/Tile.java
+++ src/org/pandcorps/pandax/tile/Tile.java
@@ -70,6 +70,11 @@ public final class Tile {
         this.background = background;
     }
     
+    public final void setBackground(final TileMapImage background, final boolean solid) {
+        setBackground(background);
+        setSolid(solid);
+    }
+    
     /*package*/ final void setBackgroundO(final Object background) {
         this.background = background;
     }
