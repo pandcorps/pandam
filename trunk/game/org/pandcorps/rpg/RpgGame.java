ad472905dc527c5b35fc96ab2d7100e889bbe672
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2013-04-02 15:39:27 +0000

    Simplified code.

--- trunk/game/org/pandcorps/rpg/RpgGame.java
+++ trunk/game/org/pandcorps/rpg/RpgGame.java
@@ -89,53 +89,34 @@ public class RpgGame extends BaseGame {
 		final TileMapImage[][] imgMap = tm.splitImageMap();
 		tm.setTileListener(new QuaintTileListener(imgMap));
 		tm.fillBackground(imgMap[5][0]);
-		tm.getTile(6, 5).setBackground(imgMap[4][1]);
-		tm.getTile(6, 5).setSolid(true);
-		tm.getTile(6, 6).setBackground(imgMap[4][1]);
-		tm.getTile(6, 6).setSolid(true);
-		tm.getTile(6, 7).setBackground(imgMap[3][1]);
-		tm.getTile(6, 7).setSolid(true);
-		tm.getTile(6, 8).setBackground(imgMap[2][1]);
-		tm.getTile(6, 8).setSolid(true);
-		tm.getTile(6, 9).setBackground(imgMap[1][1]);
-		tm.getTile(6, 9).setSolid(true);
+		tm.getTile(6, 5).setBackground(imgMap[4][1], true);
+		tm.getTile(6, 6).setBackground(imgMap[4][1], true);
+		tm.getTile(6, 7).setBackground(imgMap[3][1], true);
+		tm.getTile(6, 8).setBackground(imgMap[2][1], true);
+		tm.getTile(6, 9).setBackground(imgMap[1][1], true);
 		tm.getTile(6, 10).setForeground(imgMap[0][1]);
-		tm.getTile(7, 5).setBackground(imgMap[3][4]); // Window
-		tm.getTile(7, 5).setSolid(true);
-		tm.getTile(7, 6).setBackground(imgMap[4][2]);
-		tm.getTile(7, 6).setSolid(true);
-		tm.getTile(7, 7).setBackground(imgMap[3][2]);
-		tm.getTile(7, 7).setSolid(true);
-		tm.getTile(7, 8).setBackground(imgMap[2][2]);
-		tm.getTile(7, 8).setSolid(true);
-		tm.getTile(7, 9).setBackground(imgMap[1][2]);
-		tm.getTile(7, 9).setSolid(true);
+		tm.getTile(7, 5).setBackground(imgMap[3][4], true); // Window
+		tm.getTile(7, 6).setBackground(imgMap[4][2], true);
+		tm.getTile(7, 7).setBackground(imgMap[3][2], true);
+		tm.getTile(7, 8).setBackground(imgMap[2][2], true);
+		tm.getTile(7, 9).setBackground(imgMap[1][2], true);
 		tm.getTile(7, 10).setForeground(imgMap[0][2]);
-		tm.getTile(8, 5).setBackground(imgMap[4][3]);
-		tm.getTile(8, 5).setSolid(true);
-		tm.getTile(8, 6).setBackground(imgMap[4][3]);
-		tm.getTile(8, 6).setSolid(true);
-		tm.getTile(8, 7).setBackground(imgMap[3][3]);
-		tm.getTile(8, 7).setSolid(true);
-		tm.getTile(8, 8).setBackground(imgMap[2][3]);
-		tm.getTile(8, 8).setSolid(true);
-		tm.getTile(8, 9).setBackground(imgMap[1][3]);
-		tm.getTile(8, 9).setSolid(true);
+		tm.getTile(8, 5).setBackground(imgMap[4][3], true);
+		tm.getTile(8, 6).setBackground(imgMap[4][3], true);
+		tm.getTile(8, 7).setBackground(imgMap[3][3], true);
+		tm.getTile(8, 8).setBackground(imgMap[2][3], true);
+		tm.getTile(8, 9).setBackground(imgMap[1][3], true);
 		tm.getTile(8, 10).setForeground(imgMap[0][1]);
 		for (int i = 9; i <= 13; i++) {
-    		tm.getTile(i, 7).setBackground(imgMap[4][2]);
-            tm.getTile(i, 7).setSolid(true);
-            tm.getTile(i, 8).setBackground(imgMap[4][2]);
-            tm.getTile(i, 8).setSolid(true);
-            tm.getTile(i, 9).setBackground(imgMap[2][4]);
-            tm.getTile(i, 9).setSolid(true);
+    		tm.getTile(i, 7).setBackground(imgMap[4][2], true);
+            tm.getTile(i, 8).setBackground(imgMap[4][2], true);
+            tm.getTile(i, 9).setBackground(imgMap[2][4], true);
             tm.getTile(i, 10).setForeground(imgMap[0][1]);
 		}
 		tm.getTile(12, 7).setBackground(imgMap[0][5]); // Sign
 		tm.getTile(14, 7).setBackground(imgMap[1][6]); // Shadow
 		tm.getTile(14, 8).setBackground(imgMap[0][6]);
-		tm.getTile(4, 8).setBackground(imgMap[1][0]); // Tree
-		tm.getTile(4, 8).setSolid(true);
+		tm.getTile(4, 8).setBackground(imgMap[1][0], true); // Tree
 		tm.getTile(4, 9).setForeground(imgMap[0][0]);
 		tm.getTile(7, 3).setBackground(imgMap[2][0]); // Many flowers
 		tm.getTile(9, 1).setBackground(imgMap[3][0]); // Some flowers
@@ -143,39 +124,24 @@ public class RpgGame extends BaseGame {
 		tm.getTile(12, 2).setBackground(imgMap[4][0]); // Dirt patch
 		tm.getTile(2, 5).setBackground(imgMap[2][6]);
 		tm.getTile(3, 5).setBackground(imgMap[2][6]);
-		tm.getTile(1, 4).setBackground(imgMap[5][1]); // Water
-		tm.getTile(1, 4).setSolid(true);
-		tm.getTile(2, 4).setBackground(imgMap[5][5]);
-		tm.getTile(2, 4).setSolid(true);
-		tm.getTile(3, 4).setBackground(imgMap[5][5]);
-		tm.getTile(3, 4).setSolid(true);
-		tm.getTile(4, 4).setBackground(imgMap[5][2]);
-		tm.getTile(4, 4).setSolid(true);
+		tm.getTile(1, 4).setBackground(imgMap[5][1], true); // Water
+		tm.getTile(2, 4).setBackground(imgMap[5][5], true);
+		tm.getTile(3, 4).setBackground(imgMap[5][5], true);
+		tm.getTile(4, 4).setBackground(imgMap[5][2], true);
 		tm.getTile(0, 3).setBackground(imgMap[4][6]);
-		tm.getTile(1, 3).setBackground(imgMap[5][5]);
-		tm.getTile(1, 3).setSolid(true);
-		tm.getTile(2, 3).setBackground(imgMap[5][5]);
-		tm.getTile(2, 3).setSolid(true);
-		tm.getTile(3, 3).setBackground(imgMap[5][5]);
-		tm.getTile(3, 3).setSolid(true);
-		tm.getTile(4, 3).setBackground(imgMap[5][5]);
-		tm.getTile(4, 3).setSolid(true);
+		tm.getTile(1, 3).setBackground(imgMap[5][5], true);
+		tm.getTile(2, 3).setBackground(imgMap[5][5], true);
+		tm.getTile(3, 3).setBackground(imgMap[5][5], true);
+		tm.getTile(4, 3).setBackground(imgMap[5][5], true);
 		tm.getTile(5, 3).setBackground(imgMap[3][6]);
 		tm.getTile(0, 2).setBackground(imgMap[4][6]);
-		tm.getTile(1, 2).setBackground(imgMap[5][5]);
-		tm.getTile(1, 2).setSolid(true);
-		tm.getTile(2, 2).setBackground(imgMap[5][5]);
-		tm.getTile(2, 2).setSolid(true);
-		tm.getTile(3, 2).setBackground(imgMap[5][5]);
-		tm.getTile(3, 2).setSolid(true);
-		tm.getTile(4, 2).setBackground(imgMap[5][4]);
-		tm.getTile(4, 2).setSolid(true);
-		tm.getTile(1, 1).setBackground(imgMap[5][3]);
-		tm.getTile(1, 1).setSolid(true);
-		tm.getTile(2, 1).setBackground(imgMap[5][5]);
-		tm.getTile(2, 1).setSolid(true);
-		tm.getTile(3, 1).setBackground(imgMap[5][4]);
-		tm.getTile(3, 1).setSolid(true);
+		tm.getTile(1, 2).setBackground(imgMap[5][5], true);
+		tm.getTile(2, 2).setBackground(imgMap[5][5], true);
+		tm.getTile(3, 2).setBackground(imgMap[5][5], true);
+		tm.getTile(4, 2).setBackground(imgMap[5][4], true);
+		tm.getTile(1, 1).setBackground(imgMap[5][3], true);
+		tm.getTile(2, 1).setBackground(imgMap[5][5], true);
+		tm.getTile(3, 1).setBackground(imgMap[5][4], true);
 		tm.getTile(2, 0).setBackground(imgMap[5][6]);
 		tm.getTile(10, 6).setBackground(imgMap[3][7]); // Path
 		tm.getTile(10, 5).setBackground(imgMap[2][7]);
