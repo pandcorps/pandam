39798f48a07850169d9c10ced02fb978a2acba10
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2013-04-18 14:43:34 +0000

    Made use of more available resources.

--- trunk/game/org/pandcorps/rpg/Npc.java
+++ trunk/game/org/pandcorps/rpg/Npc.java
@@ -25,6 +25,8 @@ package org.pandcorps.rpg;
 import java.awt.image.*;
 
 import org.pandcorps.core.*;
+import org.pandcorps.core.img.*;
+import org.pandcorps.core.img.ReplacePixelFilter;
 import org.pandcorps.game.actor.*;
 import org.pandcorps.game.actor.Guy4Controller.NpcController;
 import org.pandcorps.game.core.*;
@@ -32,6 +34,11 @@ import org.pandcorps.pandam.*;
 import org.pandcorps.pandax.tile.*;
 
 public class Npc extends Character {
+    private final static FinPancolor col1 = new FinPancolor((short) 140);
+    private final static FinPancolor col2 = new FinPancolor((short) 160);
+    private final static FinPancolor col3 = new FinPancolor((short) 180);
+    private final static FinPancolor col4 = new FinPancolor((short) 200);
+    
     private final String name = "GUY";
     private NpcController controller = null;
     
@@ -44,16 +51,21 @@ public class Npc extends Character {
         final BufferedImage[] body = ImtilX.loadStrip("org/pandcorps/rpg/res/chr/MBody.png");
         final BufferedImage[] face = ImtilX.loadStrip("org/pandcorps/rpg/res/chr/MFace01.png", 8, false);
         final BufferedImage[] eyes = ImtilX.loadStrip("org/pandcorps/rpg/res/chr/Eyes01.png", 8, false);
+        final PixelFilter skinFilter = getFilter(new Pancolor(180, 130, 90), new Pancolor(200, 150, 110), new Pancolor(220, 170, 130), new Pancolor(240, 190, 150));
         for (int i = 0; i < 5; i += 4) {
             Imtil.copy(face[0], body[i], 0, 0, 8, 8, 4, 1, Imtil.COPY_FOREGROUND);
+            body[i] = Imtil.filter(body[i], skinFilter);
             Imtil.copy(eyes[0], body[i], 0, 0, 8, 4, 4, 5, Imtil.COPY_FOREGROUND);
             Imtil.copy(face[1], body[i + 1], 0, 0, 8, 8, 4, 1, Imtil.COPY_FOREGROUND);
+            body[i + 1] = Imtil.filter(body[i + 1], skinFilter);
             Imtil.copy(eyes[1], body[i + 1], 0, 0, 8, 4, 5, 5, Imtil.COPY_FOREGROUND);
+            body[i + 2] = Imtil.filter(body[i + 2], skinFilter);
         }
         Imtil.mirror(face[1]);
         Imtil.mirror(eyes[1]);
         for (int i = 0; i < 5; i += 4) {
             Imtil.copy(face[1], body[i + 3], 0, 0, 8, 8, 4, 1, Imtil.COPY_FOREGROUND);
+            body[i + 3] = Imtil.filter(body[i + 3], skinFilter);
             Imtil.copy(eyes[1], body[i + 3], 0, 0, 8, 4, 3, 5, Imtil.COPY_FOREGROUND);
         }
         final Panmage[] sheet = new Panmage[8];
@@ -62,6 +74,15 @@ public class Npc extends Character {
             sheet[i] = engine.createImage(Pantil.vmid(), o, null, null, body[i]);
         }
         return sheet;
+    }
+    
+    private final static PixelFilter getFilter(final Pancolor c1, final Pancolor c2, final Pancolor c3, final Pancolor c4) {
+        final ReplacePixelFilter filter = new ReplacePixelFilter();
+        filter.put(col1, c1);
+        filter.put(col2, c2);
+        filter.put(col3, c3);
+        filter.put(col4, c4);
+        return filter;
     }
     
     @Override
