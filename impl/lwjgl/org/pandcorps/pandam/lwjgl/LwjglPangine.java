380edf1b9bd3e605d25a9eb6fbe17918c28f604f
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2013-06-02 00:31:38 +0000

    Modified the engine to stop music when the game exits.

--- impl/lwjgl/org/pandcorps/pandam/lwjgl/LwjglPangine.java
+++ impl/lwjgl/org/pandcorps/pandam/lwjgl/LwjglPangine.java
@@ -557,5 +557,6 @@ public final class LwjglPangine extends Pangine {
 		running = false;
 		GL11.glDisableClientState(GL11.GL_VERTEX_ARRAY);
         GL11.glDisableClientState(GL11.GL_TEXTURE_COORD_ARRAY);
+        getMusic().end();
 	}
 }
