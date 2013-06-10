cd007867ebed78b5d416a588a27f4774a981c1d7
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2013-06-10 20:15:46 +0000

    Added utility methods.

---
+++ src/org/pandcorps/core/img/MultiplyPixelFilter.java
@@ -0,0 +1,59 @@
+/*
+Copyright (c) 2009-2011, Andrew M. Martin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+   disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of Pandam nor the names of its contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.pandcorps.core.img;
+
+import org.pandcorps.core.img.Pancolor.Channel;
+
+public class MultiplyPixelFilter extends ColorPixelFilter {
+	private final static class Source {
+		private final Channel channel;
+		private final float multiplier;
+		
+		private Source(final Channel channel, final float multiplier) {
+			this.channel = channel;
+			this.multiplier = multiplier;
+		}
+	}
+	
+    private final Source rsrc;
+    private final Source gsrc;
+    private final Source bsrc;
+    
+    public MultiplyPixelFilter(final Channel rc, final float rm, final Channel gc, final float gm, final Channel bc, final float bm) {
+        this.rsrc = new Source(rc, rm);
+        this.gsrc = new Source(gc, gm);
+        this.bsrc = new Source(bc, bm);
+    }
+    
+    @Override
+    public final void filter() {
+        final short r = get(rsrc), g = get(gsrc), b = get(bsrc); // Get all before we change them
+        c.setR(r);
+        c.setG(g);
+        c.setB(b);
+    }
+    
+    private final short get(final Source src) {
+    	return (short) Math.min(Math.round(c.get(src.channel) * src.multiplier), Pancolor.MAX_VALUE);
+    }
+}
