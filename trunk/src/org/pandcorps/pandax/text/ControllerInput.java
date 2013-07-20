b52090ec448c74380a97d52f77d1d2ccde5bdd3b
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2013-07-20 15:04:58 +0000

    Added utility methods.

---
+++ trunk/src/org/pandcorps/pandax/text/ControllerInput.java
@@ -0,0 +1,92 @@
+/*
+Copyright (c) 2009-2011, Andrew M. Martin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+   disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of Pandam nor the names of its contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.pandcorps.pandax.text;
+
+import org.pandcorps.pandam.Pangine;
+import org.pandcorps.pandam.Panput;
+import org.pandcorps.pandam.Panteraction;
+import org.pandcorps.pandam.event.action.ActionStartEvent;
+import org.pandcorps.pandam.event.action.ActionStartListener;
+
+public final class ControllerInput extends Input {
+	private final static char[] UPPER
+		= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	
+	private final char[] chars = UPPER;
+	private int index;
+	private int value;
+	
+	public ControllerInput(final Font font, final InputSubmitListener listener) {
+		super(font, listener);
+	}
+	
+	public ControllerInput(final MultiFont fonts, final InputSubmitListener listener) {
+		super(fonts, listener);
+	}
+	
+	@Override
+    protected final void focus() {
+		index = buf.length();
+		value = chars.length;
+		final Pangine engine = Pangine.getEngine();
+        final Panteraction interaction = engine.getInteraction();
+        final Panput submit = interaction.KEY_SPACE, up = interaction.KEY_UP, down = interaction.KEY_DOWN;
+        Panput.inactivate(submit, up, down);
+        final ActionStartListener upListener = new ActionStartListener() {
+            @Override
+            public void onActionStart(final ActionStartEvent event) {
+            	if (value == chars.length) {
+            		value = 0;
+            		buf.append(' ');
+            	} else {
+            		value++;
+            	}
+            	if (value != chars.length) {
+            		buf.setCharAt(index, chars[value]);
+            	} else {
+            		buf.setLength(index);
+            	}
+                //submit(changeListener);
+            }};
+        final ActionStartListener downListener = new ActionStartListener() {
+            @Override
+            public void onActionStart(final ActionStartEvent event) {
+            	if (value == chars.length) {
+            		buf.append(' ');
+            	} else if (value == 0) {
+            		value = chars.length;
+            	} else {
+            		value--;
+            	}
+            	if (value != chars.length) {
+            		buf.setCharAt(index, chars[value]);
+            	} else {
+            		buf.setLength(index);
+            	}
+                //submit(changeListener);
+            }};
+        //label.register(submit, submitListener);
+        label.register(up, upListener);
+        label.register(down, downListener);
+	}
+}
