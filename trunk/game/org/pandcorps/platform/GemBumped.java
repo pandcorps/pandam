59243e93198cb9c6a6358441e3badd757ed53d65
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2013-10-16 14:22:00 +0000

    Minor tweaks.

--- trunk/game/org/pandcorps/platform/GemBumped.java
+++ trunk/game/org/pandcorps/platform/GemBumped.java
@@ -22,39 +22,76 @@ POSSIBILITY OF SUCH DAMAGE.
 */
 package org.pandcorps.platform;
 
-import org.pandcorps.game.core.ImtilX;
+import org.pandcorps.core.*;
+import org.pandcorps.game.core.*;
 import org.pandcorps.pandam.*;
 import org.pandcorps.pandam.event.*;
-import org.pandcorps.pandax.Pandy;
+import org.pandcorps.pandax.*;
 import org.pandcorps.pandax.tile.Tile;
 
 public class GemBumped extends Pandy {
-    private final boolean award;
+    protected final static int AWARD_DEF = 1;
+    private final static int AWARD_2 = AWARD_DEF * 10;
+    private final static int AWARD_3 = AWARD_2 * 10;
+    private final static int AWARD_4 = AWARD_3 * 10;
+    private final static int AWARD_LEVEL = AWARD_2;
+    private final int award;
 	private final boolean end;
 	int age = 0;
 	
 	public GemBumped(final Player player, final Tile tile) {
-		this(player, tile, PlatformGame.gemAnm);
+		this(player, tile, rndAward());
 	}
 	
-	public GemBumped(final Player player, final Tile tile, final Panimation anm) {
-		this(player, tile.getPosition(), true, anm != PlatformGame.gemAnm && !Level.isFlash(tile), anm);
+	private final static int rndAward() {
+	    final int r = Mathtil.randi(0, 9999);
+	    if (r < 1000) {
+	        return AWARD_2;
+	    } else if (r < 1100) {
+	        return AWARD_3;
+	    } else if (r < 1110) {
+            return AWARD_4;
+	    }
+	    return AWARD_DEF;
 	}
+	
+	private GemBumped(final Player player, final Tile tile, final int award) {
+	    this(player, tile, award, false, getAnm(award));
+	}
+	
+	private final static Panimation getAnm(final int award) {
+        switch (award) {
+            case AWARD_DEF : return PlatformGame.gemAnm;
+            case AWARD_2 : return PlatformGame.gemBlueAnm;
+            case AWARD_3 : return PlatformGame.gemCyanAnm;
+            case AWARD_4 : return PlatformGame.gemGreenAnm;
+        }
+        throw new IllegalArgumentException("Unexpected award amount " + award);
+    }
 	
 	public GemBumped(final Player player, final Enemy defeated) {
-		this(player, defeated.getBoundingMinimum(), true, false, PlatformGame.gemAnm);
+		this(player, defeated.getBoundingMinimum(), AWARD_DEF, false, PlatformGame.gemAnm);
+	}
+	
+	public static GemBumped newLevelEnd(final Player player, final Tile tile) {
+	    return new GemBumped(player, tile, AWARD_LEVEL, true, PlatformGame.gemLevelAnm);
 	}
 	
 	public static GemBumped newShatter(final Player player) {
-	    return new GemBumped(player, player.getBoundingMinimum(), false, false, PlatformGame.gemAnm);
+	    return new GemBumped(player, player.getBoundingMinimum(), -1, false, PlatformGame.gemAnm);
 	}
 	
-	private GemBumped(final Player player, final Panple pos, final boolean award, final boolean end, final Panimation anm) {
+	private GemBumped(final Player player, final Tile tile, final int award, final boolean end, final Panimation anm) {
+	    this(player, tile.getPosition(), award, end, anm);
+	}
+	
+	private GemBumped(final Player player, final Panple pos, final int award, final boolean end, final Panimation anm) {
 		super(Tiles.g);
 		this.award = award;
 		this.end = end;
-		if (award) {
-		    Gem.collect(player);
+		final boolean good = isGood();
+		if (good) {
+		    Gem.collect(player, award);
 		}
 		setView(anm);
 		PlatformGame.setPosition(this, pos.getX(), pos.getY() + ImtilX.DIM, PlatformGame.DEPTH_SHATTER);
@@ -62,7 +99,7 @@ public class GemBumped extends Pandy {
         PlatformGame.room.addActor(this);
         if (end) {
         	Pangine.getEngine().getMusic().playSound(Music.gemLevel);
-        } else if (award) {
+        } else if (good) {
         	Gem.playSound();
         } else {
             // gemShatter
@@ -74,12 +111,16 @@ public class GemBumped extends Pandy {
 		super.onStep(event);
 		age++;
 		if (age >= 12) {
-		    if (award) {
+		    if (isGood()) {
 		        Gem.spark(this, end);
 		    } else {
 		        Tiles.shatter(PlatformGame.gemShatter, getPosition(), true);
 		        destroy();
 		    }
 		}
+	}
+	
+	private final boolean isGood() {
+	    return award > 0;
 	}
 }
