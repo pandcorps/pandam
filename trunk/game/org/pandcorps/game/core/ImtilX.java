aad13ed6feb6905eb004cab745c011a666d75b59
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2014-03-25 11:10:02 +0000

    Added a utility method.

--- trunk/game/org/pandcorps/game/core/ImtilX.java
+++ trunk/game/org/pandcorps/game/core/ImtilX.java
@@ -140,21 +140,25 @@ public final class ImtilX {
     
     public final static Img newRight2(final int d, final Pancolor fill) {
     	final Img img = Imtil.newImage(d, d);
+    	drawRight2(img, 0, 0, d, fill);
+    	return img;
+    }
+    
+    public final static void drawRight2(final Img img, final int x, final int y, final int d, final Pancolor fill) {
     	final int t = Imtil.getDataElement(Pancolor.WHITE), b = Imtil.getDataElement(Pancolor.BLACK), f = Imtil.getDataElement(fill);
     	for (int j = d - 2; j >= 0; j--) {
-    		img.setRGB(0, j, t);
+    		img.setRGB(x, y + j, t);
     	}
-    	img.setRGB(0, d - 1, b);
+    	img.setRGB(x, y + d - 1, b);
     	for (int i = 1; i < d; i++) {
-    		final int i2 = i / 2, di2 = d - 1 - i2;
-    		img.setRGB(i, i2, t);
-    		img.setRGB(i, di2, b);
+    		final int i2 = i / 2, di2 = d - 1 - i2, xi = x + i;
+    		img.setRGB(xi, y + i2, t);
+    		img.setRGB(xi, y + di2, b);
     		for (int j = i2 + 1; j < di2; j++) {
-    			img.setRGB(i, j, f);
+    			img.setRGB(xi, y + j, f);
     		}
     	}
     	//Imtil.setPseudoTranslucent(img); // Must indent first
-    	return img;
     }
     
     public final static Img newRectangle(final int w, final int h, final Pancolor fill) {
