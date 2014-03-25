fd510be8be1e203730efefe9be27b5aa25ac706b
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2014-03-25 15:13:35 +0000

    WIP.

--- trunk/src/org/pandcorps/core/img/Pancolor.java
+++ trunk/src/org/pandcorps/core/img/Pancolor.java
@@ -36,6 +36,7 @@ public class Pancolor {
 	public final static FinPancolor YELLOW = new FinPancolor(MAX_VALUE, MAX_VALUE, MIN_VALUE);
 	public final static FinPancolor CYAN = new FinPancolor(MIN_VALUE, MAX_VALUE, MAX_VALUE);
 	public final static FinPancolor MAGENTA = new FinPancolor(MAX_VALUE, MIN_VALUE, MAX_VALUE);
+	public final static FinPancolor GRAY = new FinPancolor((short) 192, (short) 192, (short) 192);
     private short r;
     private short g;
     private short b;
