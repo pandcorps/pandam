8a4d92a565880526f5abddb3f1fd6fa3eb589e1d
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2014-08-07 14:33:42 +0000

    WIP.

--- game/org/pandcorps/platform/Achievement.java
+++ game/org/pandcorps/platform/Achievement.java
@@ -90,7 +90,7 @@ public abstract class Achievement extends FinName {
 			if (ach.isMet(pc)) {
 				achieved.add(key);
 				final int award = ach.award;
-				PlatformGame.notify(pc, ach.getName() + ", " + award + " Gem bonus");
+				PlatformGame.notify(pc, ach.getName() + ", " + award + " Gem bonus", PlatformGame.gemAchieve[0]);
 				pc.addGems(award);
 				any = true;
 			}
