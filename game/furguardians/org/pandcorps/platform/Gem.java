74284015dcee507b911ec35c9f115ce4cfe7b9be
pandcorps@gmail.com <pandcorps@gmail.com@246e0705-c451-c63f-1db1-ea4b59c79162>
2015-02-21 17:04:39 +0000

    Bug fixes.

--- game/furguardians/org/pandcorps/platform/Gem.java
+++ game/furguardians/org/pandcorps/platform/Gem.java
@@ -83,11 +83,10 @@ public class Gem extends TileOccupant implements StepListener {
 	}
 	
 	protected final static void playSound() {
-		final Pangine engine = Pangine.getEngine();
-		final long clock = engine.getClock();
-		if (clock != lastSound) {
+		final long clock = Pangine.getEngine().getClock();
+		if (clock > lastSound) {
 			PlatformGame.soundGem.startSound();
-			lastSound = clock;
+			lastSound = clock + 1;
 		}
 	}
 	
