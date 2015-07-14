4ef27f527899fd5d4dcd783ee75fe69a2330fe2f
Pandcorps <pandcorps@gmail.com>
2015-07-14 06:59:52 -0400

    Swiping.

---
+++ src/org/pandcorps/pandam/event/action/SwipeEvent.java
@@ -0,0 +1,61 @@
+/*
+Copyright (c) 2009-2014, Andrew M. Martin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+   disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of Pandam nor the names of its contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.pandcorps.pandam.event.action;
+
+public class SwipeEvent {
+    private final int oldX;
+    private final int oldY;
+    private final int newX;
+    private final int newY;
+    
+    public SwipeEvent(final int oldX, final int oldY, final int newX, final int newY) {
+        this.oldX = oldX;
+        this.oldY = oldY;
+        this.newX = newX;
+        this.newY = newY;
+    }
+    
+    public final int getOldX() {
+        return oldX;
+    }
+    
+    public final int getOldY() {
+        return oldY;
+    }
+    
+    public final int getNewX() {
+        return newX;
+    }
+    
+    public final int getNewY() {
+        return newY;
+    }
+    
+    public final int getDiffX() {
+        return newX - oldX;
+    }
+    
+    public final int getDiffY() {
+        return newY - oldY;
+    }
+}
