dd0012177e877667c7cbab2e005da9070cd1e350
Pandcorps <pandcorps@gmail.com>
2015-07-25 10:42:52 -0400

    Bowler.

--- game/furguardians/org/pandcorps/furguardians/Avatar.java
+++ game/furguardians/org/pandcorps/furguardians/Avatar.java
@@ -361,6 +361,7 @@ public class Avatar extends EyeData implements Segmented {
         new Hat("Bow", "Bow", 2500, false, false),
         new Hat("Glasses", "Glasses", 3000, false),
         new Hat("Ribbons", "Ribbons", 4000, false),
+        new Hat("Bowler", "Bowler", 5000),
         new Hat("Helm", "Helm", 50000, false),
         new Hat(HAT_CROWN, "Crown", 100000, false)
     };
