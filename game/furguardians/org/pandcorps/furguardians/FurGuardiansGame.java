a9bfd4ab072f8f19367d9ba9b02090cdd300cc66
Pandcorps <pandcorps@gmail.com>
2015-10-14 06:42:39 -0400

    Welcome message.

--- game/furguardians/org/pandcorps/furguardians/FurGuardiansGame.java
+++ game/furguardians/org/pandcorps/furguardians/FurGuardiansGame.java
@@ -199,6 +199,7 @@ public class FurGuardiansGame extends BaseGame {
 	
 	protected final static String[][] ads = {
 		{ "Thank you", "for playing" },
+		{ "You", "rock!" },
 		{ "You're", "a superstar!" },
 		{ "You're", "awesome!" },
 	    { "Like the game?", "Tell a friend" },
