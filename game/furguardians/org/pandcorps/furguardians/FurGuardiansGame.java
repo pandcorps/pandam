9757996c4896412a39a6ada1ff71784883b7efd7
Pandcorps <pandcorps@gmail.com>
2016-01-30 08:13:36 -0500

    Version number.

--- game/furguardians/org/pandcorps/furguardians/FurGuardiansGame.java
+++ game/furguardians/org/pandcorps/furguardians/FurGuardiansGame.java
@@ -50,7 +50,7 @@ import org.pandcorps.furguardians.Player.*;
 
 public class FurGuardiansGame extends BaseGame {
 	protected final static String TITLE = "Fur-Guardians"; // res/values/strings.xml/app_name
-    protected final static String VERSION = "1.18.0"; // AndroidManifest.xml/versionName
+    protected final static String VERSION = "1.19.0"; // AndroidManifest.xml/versionName
     protected final static String YEAR = "2014-2016";
     protected final static String AUTHOR = "Andrew M. Martin";
 	/*
