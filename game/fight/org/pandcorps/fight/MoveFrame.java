c4dd672cc1d1ae43a71f2da3c17706d24c278267
Pandcorps <pandcorps@gmail.com>
2016-02-20 10:10:19 -0500

    License.

--- game/fight/org/pandcorps/fight/MoveFrame.java
+++ game/fight/org/pandcorps/fight/MoveFrame.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2009-2014, Andrew M. Martin
+Copyright (c) 2009-2016, Andrew M. Martin
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
@@ -23,7 +23,7 @@ POSSIBILITY OF SUCH DAMAGE.
 package org.pandcorps.fight;
 
 import org.pandcorps.pandam.*;
-import org.pandcorps.pandam.impl.FinPanple;
+import org.pandcorps.pandam.impl.*;
 
 public final class MoveFrame {
     /*package*/ final Panframe pframe;
