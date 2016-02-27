fb7edd60e373a50bf9f2b3e5184db1d3bd455a75
Pandcorps <pandcorps@gmail.com>
2016-02-27 16:44:29 -0500

    License.

--- test/org/pandcorps/test/pandam/TestPanmlGame.java
+++ test/org/pandcorps/test/pandam/TestPanmlGame.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2009-2014, Andrew M. Martin
+Copyright (c) 2009-2016, Andrew M. Martin
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
@@ -23,7 +23,7 @@ POSSIBILITY OF SUCH DAMAGE.
 package org.pandcorps.test.pandam;
 
 import org.pandcorps.pandam.*;
-import org.pandcorps.pandam.impl.FinPanple;
+import org.pandcorps.pandam.impl.*;
 
 public final class TestPanmlGame extends PandamTest {
 	public final void testInit() {
