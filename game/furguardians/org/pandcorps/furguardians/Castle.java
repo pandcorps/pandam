424058bccb44ad06ab81f03ad037c8a965b4893b
Pandcorps <pandcorps@gmail.com>
2016-05-29 09:47:51 -0400

    Utility code.

--- game/furguardians/org/pandcorps/furguardians/Castle.java
+++ game/furguardians/org/pandcorps/furguardians/Castle.java
@@ -64,6 +64,7 @@ public class Castle {
             room.addActor(tm);
             
             Menu.PlayerScreen.registerBackPromptQuit(tm);
+            Player.registerCaptureScreen(tm);
             draw();
             FurGuardiansGame.fadeIn(room);
             getMusic().changeMusic();
