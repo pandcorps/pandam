a8c53f1c9dfeda710602cf7beb28e91a16075f84
Pandcorps <pandcorps@gmail.com>
2016-06-25 14:36:54 -0400

    Gems.

--- game-furguardians/src/main/java/org/pandcorps/furguardians/GemScreen.java
+++ game-furguardians/src/main/java/org/pandcorps/furguardians/GemScreen.java
@@ -35,6 +35,8 @@ public final class GemScreen extends MiniGameScreen {
     private final static int DIM = 16;
     private final static int SIZE = 6;
     private final static int NUM_COLORS = 4;
+    private final static int TYPE_NORMAL = 0;
+    private final static int TYPE_BREAK = 1;
     private static Panmage gemTiles = null;
     private static Panmage gemTiles2 = null;
     private static Panmage gemTiles3 = null;
@@ -49,6 +51,7 @@ public final class GemScreen extends MiniGameScreen {
     TODO
     Profile setting for last mini-game played; Menu should default to that.
     Statistics for biggest gem block, most tiles cleared in one move, Gem-games played.
+    Initial shuffle can't allow diamonds beside matching gems or big gem blocks.
     */
     
     @Override
@@ -97,20 +100,38 @@ public final class GemScreen extends MiniGameScreen {
         validSelection = true;
     }
     
+    private final static TileMapImage getImage(final int color, final int type) {
+        final int row = color/ 2, col = color % 2, rowOff, colOff = 0;
+        rowOff = (type == TYPE_NORMAL) ? 0 : 1;
+        return imgMap[row * 3 + rowOff][col * 4 + colOff];
+    }
+    
     private final static void buildGrid() {
         final int area = SIZE * SIZE, perColor = area / NUM_COLORS;
-        final List<TileMapImage> list = new ArrayList<TileMapImage>(area);
+        final int[] list = new int[area];
         for (int color = 0; color < NUM_COLORS; color++) {
-            final int row = color/ 2, col = color % 2;
-            final TileMapImage img = imgMap[row * 3][col * 4];
+            final int row = color * perColor;
             for (int i = 0; i < perColor; i++) {
-                list.add(img);
+                list[row + i] = color;
             }
         }
-        Collections.shuffle(list);
+        while (true) {
+            Mathtil.shuffle(list);
+            if (findBigBlocks(list)) {
+                continue;
+            } else if (!initBreaks(list)) {
+                continue;
+            }
+            break;
+        }
         int i = 0, j = 0;
-        for (final TileMapImage img : list) {
-            grid[j][i] = new Cell(i, j, img);
+        for (int color : list) {
+            int type = TYPE_NORMAL;
+            if (color >= NUM_COLORS) {
+                color -= NUM_COLORS;
+                type = TYPE_BREAK;
+            }
+            grid[j][i] = new Cell(i, j, color, type);
             i++;
             if (i >= SIZE) {
                 i = 0;
@@ -119,8 +140,33 @@ public final class GemScreen extends MiniGameScreen {
         }
     }
     
+    private final static boolean findBigBlocks(final int[] list) {
+        return false; //TODO
+    }
+    
+    private final static boolean initBreaks(final int[] list) {
+        final int size = list.length, breaks[] = new int[NUM_COLORS];
+        for (int color = 0; color < NUM_COLORS; color++) {
+            //TODO Start at a random spot in the list
+            for (int i = 0; i < size; i++) {
+                if (list[i] == color) {
+                    //TODO Check neighbors
+                    breaks[color] = i;
+                    break;
+                }
+            }
+        }
+        for (final int i : breaks) {
+            list[i] += NUM_COLORS;
+        }
+        return true; //TODO return false if can't find a safe spot for a break of any color
+    }
+    
     private final static void onRelease() {
-        if (currentSelection.size() == 2) {
+        final int size = currentSelection.size();
+        if (validSelection && size < 2) {
+            return;
+        } else if (size == 2) {
             swap();
         }
         clearCurrentSelection();
@@ -142,12 +188,16 @@ public final class GemScreen extends MiniGameScreen {
         private final int i;
         private final int j;
         private final TouchButton button;
+        private int color;
+        private int type;
         
-        private Cell(final int i, final int j, final TileMapImage img) {
+        private Cell(final int i, final int j, final int color, final int type) {
             this.i = i;
             this.j = j;
-            setBackground(img);
+            setBackground(getImage(color, type));
             button = newButton();
+            this.color = color;
+            this.type = type;
         }
         
         private final TouchButton newButton() {
@@ -199,8 +249,13 @@ public final class GemScreen extends MiniGameScreen {
         
         private final void swap(final Cell cell) {
             final Object bg = getBackground();
+            final int color = this.color, type = this.type;
             setBackground(cell.getBackground());
+            this.color = cell.color;
+            this.type = cell.type;
             cell.setBackground(bg);
+            cell.color = color;
+            cell.type = type;
             //TODO Spark?
         }
         
