3f2ede8612861bf0f950cf146c13681bf02106e1
Pandcorps <pandcorps@gmail.com>
2016-10-11 06:57:53 -0400

    Bots.

--- game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/RoomLoader.java
+++ game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/RoomLoader.java
@@ -50,10 +50,11 @@ public abstract class RoomLoader {
                 BotsnBoltsGame.BotsnBoltsScreen.loadTileImage(seg.getValue(0));
                 while ((seg = in.read()) != null) {
                     final String name = seg.getName();
-                    if ("RCT".equals(name)) {
+                    if ("RCT".equals(name)) { // Rectangle
                         rct(seg.intValue(0), seg.intValue(1), seg.intValue(2), seg.intValue(3), seg, 4);
-                    } else if ("LDR".equals(name)) {
-                    } else if ("DOR".equals(name)) {
+                    } else if ("LDR".equals(name)) { // Ladder
+                    } else if ("BRR".equals(name)) { // Barrier
+                    } else if ("DOR".equals(name)) { // Door
                         dor(seg.intValue(0), seg.intValue(1), seg.getValue(2));
                     }
                 }
