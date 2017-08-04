cf4b27dab64a6cf622614c505ea3b9cd495a7668
Pandcorps <pandcorps@gmail.com>
2017-08-04 07:12:52 -0400

    Bots.

--- game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/Boss.java
+++ game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/Boss.java
@@ -915,6 +915,7 @@ public abstract class Boss extends Enemy {
 
         @Override
         protected final boolean continueState() {
+            startStill();
             return false;
         }
 
