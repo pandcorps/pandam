6f0253c9e616142aca974d07ea8dc624bfb90559
Pandcorps <pandcorps@gmail.com>
2017-10-04 06:44:50 -0400

    Bots.

--- game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/Boss.java
+++ game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/Boss.java
@@ -1241,6 +1241,7 @@ public abstract class Boss extends Enemy {
         protected static Panmage jumpDrill2 = null;
         protected static Panmage jumpDrill3 = null;
         protected static Panmage drill1 = null;
+        protected static Panmage drill2 = null;
         private int drillTimer = -1;
         
         protected EarthquakeBot(final int x, final int y) {
