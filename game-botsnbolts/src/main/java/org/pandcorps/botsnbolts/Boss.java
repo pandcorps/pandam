6305873a0c387e886ee6e385140f6dc8d5a9f551
Pandcorps <pandcorps@gmail.com>
2017-10-26 06:44:10 -0400

    Bots.

--- game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/Boss.java
+++ game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/Boss.java
@@ -1581,6 +1581,10 @@ public abstract class Boss extends Enemy {
             return (whirl1 = getCycloneImage(whirl1, "cyclonebot/CycloneBotWhirl1"));
         }
         
+        protected final static Panmage getWhirl2() {
+            return (whirl2 = getCycloneImage(whirl2, "cyclonebot/CycloneBotWhirl2"));
+        }
+        
         protected final static Panmage getCycloneImage(final Panmage img, final String name) {
             return getImage(img, name, CYCLONE_O, CYCLONE_MIN, CYCLONE_MAX);
         }
