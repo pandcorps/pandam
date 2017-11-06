ee142366bf40ae68d422cc99bb4533947ec7b693
Pandcorps <pandcorps@gmail.com>
2017-11-06 07:15:47 -0500

    Bots.

--- game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/RoomFunction.java
+++ game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/RoomFunction.java
@@ -22,6 +22,8 @@ POSSIBILITY OF SUCH DAMAGE.
 */
 package org.pandcorps.botsnbolts;
 
+import org.pandcorps.botsnbolts.RoomLoader.*;
+import org.pandcorps.pandam.*;
 import org.pandcorps.pandax.tile.*;
 import org.pandcorps.pandax.tile.Tile.*;
 
@@ -42,6 +44,64 @@ public abstract class RoomFunction {
             for (int j = y - 1; j >= 0; j--) {
                 setOverlayIfOpen(tm, x, j, left, Tile.BEHAVIOR_OPEN);
                 setOverlayIfOpen(tm, x1, j, right, Tile.BEHAVIOR_OPEN);
+            }
+        }
+    }
+    
+    public final static class BigFan extends RoomFunction {
+        private final TileMapImage topLeft = BotsnBoltsGame.imgMap[5][4];
+        private final TileMapImage bottomLeft = BotsnBoltsGame.imgMap[6][4];
+        private final TileMapImage topRight = new AdjustedTileMapImage(topLeft, 0, true, false);
+        private final TileMapImage bottomRight = new AdjustedTileMapImage(bottomLeft, 0, true, false);
+        
+        private final TileMapImage topLeft1 = new AdjustedTileMapImage(topLeft, 1, false, false);
+        private final TileMapImage bottomLeft1 = new AdjustedTileMapImage(bottomLeft, 1, false, false);
+        private final TileMapImage topRight1 = new AdjustedTileMapImage(topLeft, 1, false, true);
+        private final TileMapImage bottomRight1 = new AdjustedTileMapImage(bottomLeft, 1, false, true);
+        
+        private final TileMapImage topLeft2 = new AdjustedTileMapImage(topLeft, 2, false, false);
+        private final TileMapImage bottomLeft2 = new AdjustedTileMapImage(bottomLeft, 2, false, false);
+        private final TileMapImage topRight2 = new AdjustedTileMapImage(topLeft, 2, true, false);
+        private final TileMapImage bottomRight2 = new AdjustedTileMapImage(bottomLeft, 2, true, false);
+        
+        private final TileMapImage topLeft3 = new AdjustedTileMapImage(topLeft, 3, false, false);
+        private final TileMapImage bottomLeft3 = new AdjustedTileMapImage(bottomLeft, 3, false, false);
+        private final TileMapImage topRight3 = new AdjustedTileMapImage(topLeft, 3, false, true);
+        private final TileMapImage bottomRight3 = new AdjustedTileMapImage(bottomLeft, 3, false, true);
+        
+        private final TileMapImage poleLeft = BotsnBoltsGame.imgMap[7][4];
+        private final TileMapImage poleRight = new AdjustedTileMapImage(poleLeft, 0, true, false);
+        
+        private static Panroom lastRoom = null;
+        
+        @Override
+        public final void build(final TileMap tm, final int x, final int y) {
+            final Tile tileTopLeft = tm.getTile(null, topLeft, Tile.BEHAVIOR_OPEN);
+            final Tile tileBottomLeft = tm.getTile(poleLeft, bottomLeft, Tile.BEHAVIOR_OPEN);
+            final Tile tileTopRight = tm.getTile(null, topRight, Tile.BEHAVIOR_OPEN);
+            final Tile tileBottomRight = tm.getTile(poleRight, bottomRight, Tile.BEHAVIOR_OPEN);
+            final Tile tilePoleLeft = tm.getTile(poleLeft, null, Tile.BEHAVIOR_OPEN);
+            final Tile tilePoleRight = tm.getTile(poleRight, null, Tile.BEHAVIOR_OPEN);
+            final int x1 = x + 1, y1 = y - 1;
+            tm.setTile(x, y, tileTopLeft);
+            tm.setTile(x1, y, tileTopRight);
+            tm.setTile(x, y1, tileBottomLeft);
+            tm.setTile(x1, y1, tileBottomRight);
+            for (int j = 0; j < y1; j++) {
+                tm.setTile(x, j, tilePoleLeft);
+                tm.setTile(x1, j, tilePoleRight);
+            }
+            if (lastRoom != RoomLoader.nextRoom) {
+                final int d = 4;
+                RoomLoader.animators.add(new RoomLoader.TileAnimator(tileTopLeft,
+                    new TileFrame(null, topLeft, d), new TileFrame(null, topRight1, d), new TileFrame(null, bottomRight2, d), new TileFrame(null, bottomLeft3, d)));
+                RoomLoader.animators.add(new RoomLoader.TileAnimator(tileBottomLeft,
+                    new TileFrame(poleLeft, bottomLeft, d), new TileFrame(poleLeft, topLeft1, d), new TileFrame(poleLeft, topRight2, d), new TileFrame(poleLeft, bottomRight3, d)));
+                RoomLoader.animators.add(new RoomLoader.TileAnimator(tileTopRight,
+                    new TileFrame(null, topRight, d), new TileFrame(null, bottomRight1, d), new TileFrame(null, bottomLeft2, d), new TileFrame(null, topLeft3, d)));
+                RoomLoader.animators.add(new RoomLoader.TileAnimator(tileBottomRight,
+                    new TileFrame(poleRight, bottomRight, d), new TileFrame(poleRight, bottomLeft1, d), new TileFrame(poleRight, topLeft2, d), new TileFrame(poleRight, topRight3, d)));
+                lastRoom = RoomLoader.nextRoom;
             }
         }
     }
