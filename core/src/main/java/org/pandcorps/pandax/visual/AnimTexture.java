349bb2615c3a3e2c7fbe3156a8c07e8e2239fa29
Pandcorps <pandcorps@gmail.com>
2017-11-15 06:37:43 -0500

    Utility code.

---
+++ core/src/main/java/org/pandcorps/pandax/visual/AnimTexture.java
@@ -0,0 +1,58 @@
+/*
+Copyright (c) 2009-2017, Andrew M. Martin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+   disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of Pandam nor the names of its contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.pandcorps.pandax.visual;
+
+import java.util.*;
+
+import org.pandcorps.pandam.*;
+import org.pandcorps.pandam.event.*;
+
+public class AnimTexture extends Pantexture implements StepListener {
+    private final int frameDuration;
+    private final List<Panmage> images;
+    private int timer = 0;
+    private int index = 0;
+    
+    public AnimTexture(final int frameDuration, final Panmage... images) {
+        this(frameDuration, Arrays.asList(images));
+    }
+    
+    public AnimTexture(final int frameDuration, final List<Panmage> images) {
+        this.frameDuration = frameDuration;
+        this.images = images;
+        setImage(images.get(0));
+    }
+    
+    @Override
+    public void onStep(final StepEvent event) {
+        timer++;
+        if (timer >= frameDuration) {
+            index++;
+            if (index >= images.size()) {
+                index = 0;
+            }
+            setImage(images.get(index));
+            timer = 0;
+        }
+    }
+}
