6c1aeffcdc2a2ab3aaac557fb33d88f32ff77218
Pandcorps <pandcorps@gmail.com>
2018-02-26 16:25:55 -0500

    License.

--- game-furguardians/src/main/java/org/pandcorps/furguardians/Player.java
+++ game-furguardians/src/main/java/org/pandcorps/furguardians/Player.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2009-2017, Andrew M. Martin
+Copyright (c) 2009-2018, Andrew M. Martin
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
@@ -473,9 +473,9 @@ public class Player extends Character implements CollisionListener {
 			register(interaction.KEY_2, new ActionStartListener() {
 	            @Override public final void onActionStart(final ActionStartEvent event) { right(); }});
 			register(interaction.KEY_9, new ActionStartListener() {
-	            @Override public final void onActionStart(final ActionStartEvent event) { addX(-1); }});
+	            @Override public final void onActionStart(final ActionStartEvent event) { addX(-1, true); }});
 	        register(interaction.KEY_0, new ActionStartListener() {
-	            @Override public final void onActionStart(final ActionStartEvent event) { addX(1); }});
+	            @Override public final void onActionStart(final ActionStartEvent event) { addX(1, true); }});
 	        register(interaction.KEY_Q, new ActionStartListener() {
 	            @Override public final void onActionStart(final ActionStartEvent event) { jumpMode = MODE_NORMAL; }});
 	        register(interaction.KEY_W, new ActionStartListener() {
