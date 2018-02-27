77ab459ea03cca2f8d43f76ee03ce038a836c715
Pandcorps <pandcorps@gmail.com>
2018-02-27 17:54:55 -0500

    License.

--- game-furguardians/src/main/java/org/pandcorps/furguardians/Tiles.java
+++ game-furguardians/src/main/java/org/pandcorps/furguardians/Tiles.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2009-2017, Andrew M. Martin
+Copyright (c) 2009-2018, Andrew M. Martin
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
@@ -270,6 +270,7 @@ public class Tiles {
         player.pc.profile.stats.bumpedBlocks++;
     }
     
+    //TODO See org.pandcorps.game.actor.Diver
     public static class Faller extends Pandy implements AllOobListener {
         public Faller(final Panlayer layer, final Panmage img, final float x, final float y, final float xv, final float yv) {
             super(g);
