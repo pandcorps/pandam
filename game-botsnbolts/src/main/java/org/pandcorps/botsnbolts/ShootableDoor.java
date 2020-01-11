a903a7b14dcc6323f4223f8586d288b1a7ce5d12
pandcorps <pandcorps@gmail.com>
2020-01-11 08:07:13 -0500

    Sound effects.

--- game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/ShootableDoor.java
+++ game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/ShootableDoor.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2009-2018, Andrew M. Martin
+Copyright (c) 2009-2020, Andrew M. Martin
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
@@ -522,7 +522,6 @@ public class ShootableDoor extends Panctor implements StepListener, CollisionLis
                 if (base < 0) {
                     base = 0;
                     vel = 0;
-                    BotsnBoltsGame.musicBoss.startMusic();
                 } else if (base > h) {
                     base = h;
                     vel = 0;
