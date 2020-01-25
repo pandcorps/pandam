49acae0661b61b6e8516861b86f60d7bb82cccba
pandcorps <pandcorps@gmail.com>
2020-01-25 07:16:06 -0500

    Sound effects.

--- game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/RoomFunction.java
+++ game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/RoomFunction.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2009-2018, Andrew M. Martin
+Copyright (c) 2009-2020, Andrew M. Martin
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
@@ -351,10 +351,16 @@ public abstract class RoomFunction {
             final int fadeStart = totalPeriod - (fadeFrameDuration * 3);
             if (i >= fadeStart) {
                 c = (i - (fadeStart - fadeFrameDuration)) / fadeFrameDuration;
-            } else if (i >= (fadeStart - lightningDuration)) {
-                c = 0;
             } else {
-                c = 4;
+                final int lightningStart = fadeStart - lightningDuration;
+                if (i >= lightningStart) {
+                    if (i == lightningStart) {
+                        BotsnBoltsGame.fxThunder.startSound();
+                    }
+                    c = 0;
+                } else {
+                    c = 4;
+                }
             }
             engine.setBgColor(colors[c]);
         }
