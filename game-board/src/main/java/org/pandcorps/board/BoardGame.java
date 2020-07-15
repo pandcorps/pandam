35496eefd9388f00b8a4d9b512a2d8827f994029
pandcorps <pandcorps@gmail.com>
2020-07-15 07:52:16 -0400

    WIP.

--- game-board/src/main/java/org/pandcorps/board/BoardGame.java
+++ game-board/src/main/java/org/pandcorps/board/BoardGame.java
@@ -22,6 +22,8 @@ POSSIBILITY OF SUCH DAMAGE.
 */
 package org.pandcorps.board;
 
+import java.util.*;
+
 import org.pandcorps.game.*;
 import org.pandcorps.pandam.*;
 
@@ -31,5 +33,158 @@ public class BoardGame extends BaseGame {
     }
     
     protected abstract static class BoardGameModule {
+        protected abstract void initGame();
+    }
+    
+    protected static class BoardGameGrid<P extends BoardGamePiece> {
+        private final int w;
+        private final int h;
+        private final int numCells;
+        private final List<P> grid;
+        private final Map<Integer, BoardGamePieceList<P>> lists = new HashMap<Integer, BoardGamePieceList<P>>();
+        
+        protected BoardGameGrid(final int dim) {
+            this(dim, dim);
+        }
+        
+        protected BoardGameGrid(final int w, final int h) {
+            this.w = w;
+            this.h = h;
+            numCells = w * h;
+            grid = new ArrayList<P>(numCells);
+            for (int i = 0; i < numCells; i++) {
+                grid.add(null);
+            }
+        }
+        
+        protected final void clear() {
+            for (int i = 0; i < numCells; i++) {
+                grid.set(i, null);
+            }
+        }
+        
+        protected final int getWidth() {
+            return w;
+        }
+        
+        protected final int getHeight() {
+            return h;
+        }
+        
+        protected final List<P> getPieces(final int player) {
+            final Integer key = Integer.valueOf(player);
+            BoardGamePieceList<P> list = lists.get(key);
+            if (list == null) {
+                list = new BoardGamePieceList<P>();
+                lists.put(key, list);
+            }
+            // Maybe track grid age in set method and BoardGamePieceList; if not stale, return externalList without rebuilding; might call this multiple times per turn
+            final List<P> internal = list.internalList;
+            for (final P piece : grid) {
+                if ((piece != null) && (piece.player == player)) {
+                    internal.add(piece);
+                }
+            }
+            return list.externalList;
+        }
+        
+        protected final P get(final int x, final int y) {
+            return isValid(x, y) ? grid.get(getIndex(x, y)) : null;
+        }
+        
+        protected final void set(final int x, final int y, final P piece) {
+            validate(x, y);
+            // Null out previous location here? Separate move method to do that?
+            grid.set(getIndex(x, y), piece);
+            piece.x = x;
+            piece.y = y;
+        }
+        
+        protected final void set(final int index, final P piece) {
+            grid.set(index, piece);
+            piece.x = getX(index);
+            piece.y = getY(index);
+        }
+        
+        /*protected final void set(final BoardGameCell cell, final P piece) {
+            set(cell.x, cell.y, piece);
+        }*/
+        
+        protected final boolean isOpen(final int x, final int y) {
+            return isValid(x, y) && (get(x, y) == null);
+        }
+        
+        protected final boolean isOpen(final int index) {
+            return grid.get(index) == null;
+        }
+        
+        /*protected final boolean isOpen(final BoardGameCell cell) {
+            return isOpen(cell.x, cell.y);
+        }*/
+        
+        protected final boolean isValid(final int x, final int y) {
+            return (x >= 0) && (x < w) && (y >= 0) && (y < h);
+        }
+        
+        protected final void validate(final int x, final int y) {
+            if (!isValid(x, y)) {
+                throw new IllegalArgumentException("Invalid position: " + x + ", " + y);
+            }
+        }
+        
+        protected final int getIndex(final int x, final int y) {
+            validate(x, y);
+            return (y * w) + x;
+        }
+        
+        protected final Integer getIndexWrapped(final int x, final int y) {
+            return Integer.valueOf(getIndex(x, y));
+        }
+        
+        protected final int getX(final int index) {
+            return index % w;
+        }
+        
+        protected final int getY(final int index) {
+            return index / w;
+        }
+    }
+    
+    // Just use Grid.getIndex instead
+    /*protected final static class BoardGameCell {
+        protected final int x;
+        protected final int y;
+        
+        protected BoardGameCell(final int x, final int y) {
+            this.x = x;
+            this.y = y;
+        }
+        
+        public final boolean equals(final Object o) {
+            if (o == this) {
+                return true;
+            } else if (o == null) {
+                return false;
+            } else if (o.getClass() != BoardGameCell.class) {
+                return false;
+            }
+            final BoardGameCell cell = (BoardGameCell) o;
+            return (x == cell.x) && (y == cell.y); 
+        }
+    }*/
+    
+    protected static class BoardGamePiece {
+        protected int player;
+        protected int x;
+        protected int y;
+        
+        protected BoardGamePiece(final int player) {
+            this.player = player;
+        }
+    }
+    
+    private final static class BoardGamePieceList<P extends BoardGamePiece> {
+        private final List<P> internalList = new ArrayList<P>();
+        private final List<P> externalList = Collections.unmodifiableList(internalList);
     }
 }
