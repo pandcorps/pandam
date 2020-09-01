ca12e8f0de2b4d7f76fffc22ee3d5bba53335c8a
pandcorps <pandcorps@gmail.com>
2020-09-01 06:45:27 -0400

    WIP.

--- game-championsofslam/src/main/java/org/pandcorps/championsofslam/ChampionsOfSlamGame.java
+++ game-championsofslam/src/main/java/org/pandcorps/championsofslam/ChampionsOfSlamGame.java
@@ -52,7 +52,7 @@ public final class ChampionsOfSlamGame extends BaseGame {
     protected final static int GAME_ROWS = 14;
     protected final static int GAME_W = GAME_COLUMNS * DIM; // 384
     protected final static int GAME_H = GAME_ROWS * DIM; // 224;
-    protected final static int INITIAL_OPPONENTS = Pantil.getProperty("org.pandcorps.championsofslam.opponents", 30);
+    protected final static int INITIAL_OPPONENTS = Pantil.getProperty("org.pandcorps.championsofslam.opponents", 1);
     
     protected static Queue<Runnable> loaders = new LinkedList<Runnable>();
     protected static Panroom room = null;
@@ -62,18 +62,6 @@ public final class ChampionsOfSlamGame extends BaseGame {
     protected static Panmage imgChampion = null;
     protected static Pansound soundJab = null;
     protected static Pansound soundUppercut = null;
-    protected final static ClothingStyle[] shirtStyles = {
-            new ShirtStyle(-1),
-            new ShirtStyle(32),
-    };
-    protected final static ClothingStyle[] pantsStyles = {
-            new PantsStyle(64),
-            new PantsStyle(160),
-            new PantsStyle(128)
-    };
-    protected final static ClothingStyle[] bootsStyles = {
-            new ClothingStyle(0, 96)
-    };
     protected static Panmage boundingBox = null;
     protected static boolean paused = true;
     private static boolean initialized = false;
@@ -118,6 +106,7 @@ public final class ChampionsOfSlamGame extends BaseGame {
         imgArena = engine.createImage(PRE_IMG + "arena", RES + "Arena.png");
         imgChampion = engine.createImage(PRE_IMG + "champion", RES + "Champion.png");
         boundingBox = engine.createEmptyImage(PRE_IMG + "boundingBox", null, new FinPanple2(-10, -2), new FinPanple2(10, 5));
+        Images.load();
         loadSounds();
         findFiles();
     }
