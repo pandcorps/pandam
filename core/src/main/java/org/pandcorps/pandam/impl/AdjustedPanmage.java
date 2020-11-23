7a776e0615b408baa60dc26519c48fd8cb57ff9f
pandcorps <pandcorps@gmail.com>
2020-11-23 12:32:35 -0500

    WIP.

---
+++ core/src/main/java/org/pandcorps/pandam/impl/AdjustedPanmage.java
@@ -0,0 +1,82 @@
+/*
+Copyright (c) 2009-2020, Andrew M. Martin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+   disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of Pandam nor the names of its contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.pandcorps.pandam.impl;
+
+import org.pandcorps.core.img.*;
+import org.pandcorps.pandam.*;
+
+public class AdjustedPanmage extends Panmage {
+    private Panmage src = null;
+    private final int rot;
+    private final boolean mirror;
+    private final boolean flip;
+    private final float r;
+    private final float g;
+    private final float b;
+    
+    public AdjustedPanmage(final String id, final Panmage src, final int rot, final boolean mirror, final boolean flip) {
+        this(id, src, rot, mirror, flip, 1.0f, 1.0f, 1.0f);
+    }
+    
+    public AdjustedPanmage(final String id, final Panmage src, final Pancolor color) {
+        this(id, src, color.getRf(), color.getGf(), color.getBf());
+    }
+    
+    public AdjustedPanmage(final String id, final Panmage src, final float r, final float g, final float b) {
+        this(id, src, 0, false, false, r, g, b);
+    }
+    
+    public AdjustedPanmage(final String id, final Panmage src, final int rot, final boolean mirror, final boolean flip, final float r, final float g, final float b) {
+        super(id, src.getOrigin(), src.getBoundingMinimum(), src.getBoundingMaximum());
+        this.src = src;
+        this.rot = rot;
+        this.mirror = mirror;
+        this.flip = flip;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+    
+    @Override
+    public final Panple getSize() {
+        return src.getSize();
+    }
+
+    @Override
+    protected final void render(final Panlayer layer, final float x, final float y, final float z,
+                                final float ix, final float iy, final float iw, final float ih,
+                                final int rot, final boolean mirror, final boolean flip,
+                                final float r, final float g, final float b) {
+        render(src, layer, x, y, z, ix, iy, iw, ih, (this.rot + rot) % 4, this.mirror ^ mirror, this.flip ^ flip, this.r * r, this.g * g, this.b * b);
+    }
+    
+    @Override
+    protected final void close() {
+        src = null; // Doesn't free underlying resource; it must be closed separately if needed
+    }
+    
+    @Override
+    protected final boolean isClosed() {
+        return src == null;
+    }
+}
