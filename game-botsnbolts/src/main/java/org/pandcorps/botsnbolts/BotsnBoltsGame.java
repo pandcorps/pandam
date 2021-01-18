d2c8b19d415711e2dd1e6d4eb973503129d81c67
pandcorps <pandcorps@gmail.com>
2021-01-18 08:05:14 -0500

    License.

--- game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/BotsnBoltsGame.java
+++ game-botsnbolts/src/main/java/org/pandcorps/botsnbolts/BotsnBoltsGame.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2009-2020, Andrew M. Martin
+Copyright (c) 2009-2021, Andrew M. Martin
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
@@ -52,7 +52,7 @@ import org.pandcorps.pandax.visual.*;
 public final class BotsnBoltsGame extends BaseGame {
     protected final static String TITLE = "Bots 'n Bolts";
     protected final static String VERSION = "1.0.6";
-    protected final static String YEAR = "2016-2020";
+    protected final static String YEAR = "2016-2021";
     protected final static String AUTHOR = "Andrew M. Martin";
     protected final static String COPYRIGHT = "Copyright " + Pantext.CHAR_COPYRIGHT + " " + YEAR;
     
