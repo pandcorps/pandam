cfd9aba143abf52a7c3f776f7c146ef441c7b645
pandcorps <pandcorps@gmail.com>
2021-01-21 07:26:31 -0500

    License.

--- game-fight/src/main/java/org/pandcorps/fight/FightGame.java
+++ game-fight/src/main/java/org/pandcorps/fight/FightGame.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2009-2020, Andrew M. Martin
+Copyright (c) 2009-2021, Andrew M. Martin
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
