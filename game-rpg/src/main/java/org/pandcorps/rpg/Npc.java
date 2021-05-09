da59e5cf3aa3ad43e56d5fa252785b3c301fcbb7
pandcorps <pandcorps@gmail.com>
2021-05-09 07:42:13 -0400

    RPG.

--- game-rpg/src/main/java/org/pandcorps/rpg/Npc.java
+++ game-rpg/src/main/java/org/pandcorps/rpg/Npc.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2009-2020, Andrew M. Martin
+Copyright (c) 2009-2021, Andrew M. Martin
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
@@ -26,12 +26,12 @@ import org.pandcorps.game.actor.*;
 import org.pandcorps.game.actor.Guy4Controller.*;
 import org.pandcorps.pandax.tile.*;
 
-public class Npc extends Character {
+public class Npc extends Chr {
     private final String name = "GUY";
     private NpcController controller = null;
     
-    protected Npc(final String id, final CharacterDefinition def, final NpcController controller) {
-        super(id, def);
+    protected Npc(final ChrDefinition def, final NpcController controller) {
+        super(def);
         this.controller = controller;
     }
     
